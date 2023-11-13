a025f7ad482a4fdb53273a4a06fbbb70a765d044
phiarchitect <phi@phiarchitect.com>
2023-11-13 10:07:20 -0800

    update conf add intro

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -6,29 +6,31 @@ org_name = 'GEOMETOR'
 repo = 'polynumbers'
 repo_name = 'polynumbers'
 
-blog_title = f'{org_name} • {repo_name}'
-project = f'{org_name}<br/>•<br/>{repo_name}'
-version = ''  # The short X.Y version.
-release = ''  # The full version, including alpha/beta/rc tags.
+setup_globals(org, org_name, repo, repo_name)
 
-copyright = f'{year}, {org_name}'
-author = f'{org_name}'
+#  blog_title = f'{org_name} • {repo_name}'
+#  project = f'{org_name}<br/>•<br/>{repo_name}'
+#  version = ''  # The short X.Y version.
+#  release = ''  # The full version, including alpha/beta/rc tags.
 
-# Base URL for the website, required for generating feeds.
-# e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = f'https://{org}.github.io/{repo}'
-html_base_url = blog_baseurl
-html_baseurl = blog_baseurl
+#  copyright = f'{year}, {org_name}'
+#  author = f'{org_name}'
 
-blog_authors = {
-    "phi": ("phi ARCHITECT", None),
-}
+#  # Base URL for the website, required for generating feeds.
+#  # e.g. blog_baseurl = "http://example.com/"
+#  blog_baseurl = f'https://{org}.github.io/{repo}'
+#  html_base_url = blog_baseurl
+#  html_baseurl = blog_baseurl
 
-html_theme_options = {
-    'logo': 'logo.png',
-    'logo_name': True,
-    'github_user': org,
-    'github_repo': repo,
-    'github_button': True,
-}
+#  blog_authors = {
+    #  "phi": ("phi ARCHITECT", None),
+#  }
+
+#  html_theme_options = {
+    #  'logo': 'logo.png',
+    #  'logo_name': True,
+    #  'github_user': org,
+    #  'github_repo': repo,
+    #  'github_button': True,
+#  }
 
